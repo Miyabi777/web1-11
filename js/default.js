22094fefaefb84936a3f9623415dffc02ad92d5c
Miyabi777 <kmgi77@icloud.com>
2017-06-19 17:40:57 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。０～２の数字を入力してください。'));
 
-var message
+var message;
 if(answer === number){
   message = '当たり！';
 }else if(answer < number){
